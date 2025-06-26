349f49ae1db3f91887b5b6970ca0185b6fb997ff
Ben <ben.smith.developer@gmail.com>
2025-06-26 12:18:39 -0700

    chore(deps): restrict aws provider version to < 6.0.0 (#33)

--- src/versions.tf
+++ src/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = ">= 4.0, < 6.0.0"
     }
     awsutils = {
       source  = "cloudposse/awsutils"
-      version = ">= 0.11.0"
+      version = ">= 0.11.0, < 6.0.0"
     }
     local = {
       source  = "hashicorp/local"
